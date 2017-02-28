335c31d283a91ca1d875bb1b80df9abea92a2b30
rrang004 <rrang004@ucr.edu>
2017-02-27 19:23:34 -0800

    updated command.h

--- src/Command.h
+++ src/Command.h
@@ -9,14 +9,17 @@
 #include <string.h>
 #include <fstream>
 #include <cctype>
-#include <cstring>
-#include <boost/foreach.hpp>
-#include <boost/algorithm/string.hpp>                                                                                                                                                
-#include <boost/algorithm/string/regex.hpp>                                                                                                                                          
-#include <sys/wait.h>
 using namespace std;
 
 class Command {
+    private:
+        // string paraList;
+        // const char* command;
+        // char *const parmList;
     public:
-        Command(string);
+        Command(vector<char*>);
+        // ~Command();
+        // void execute();
+        void displayCommand();
+    
 };
